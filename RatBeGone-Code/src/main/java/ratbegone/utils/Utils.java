284947a7d45846e50974591ebf5f9e4d8ad9ed1c
KillerKillsKillers <142183664+KillerKillsKillers@users.noreply.github.com>
2023-08-15 06:02:34 -0400

    Update Utils.java

--- RatBeGone-Code/src/main/java/ratbegone/utils/Utils.java
+++ RatBeGone-Code/src/main/java/ratbegone/utils/Utils.java
@@ -58,30 +58,22 @@ public class Utils {
 			new File(newPath).mkdir();
 			Enumeration zipFileEntries = zip.entries();
 
-			// Process each entry
 			while (zipFileEntries.hasMoreElements()) {
-				// grab a zip file entry
+
 				ZipEntry entry = (ZipEntry) zipFileEntries.nextElement();
 				String currentEntry = entry.getName();
-
 				File destFile = new File(newPath, currentEntry);
-				// destFile = new File(newPath, destFile.getName());
 				File destinationParent = destFile.getParentFile();
-
-				// create the parent directory structure if needed
 				destinationParent.mkdirs();
 
 				if (!entry.isDirectory()) {
 					BufferedInputStream is = new BufferedInputStream(zip.getInputStream(entry));
 					int currentByte;
-					// establish buffer for writing file
 					byte data[] = new byte[BUFFER];
 
-					// write the current file to disk
 					FileOutputStream fos = new FileOutputStream(destFile);
 					BufferedOutputStream dest = new BufferedOutputStream(fos, BUFFER);
 
-					// read and write until last byte is encountered
 					while ((currentByte = is.read(data, 0, BUFFER)) != -1) {
 						dest.write(data, 0, currentByte);
 					}
